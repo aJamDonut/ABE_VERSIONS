bb90615280c391e944c16700c95d79e3dae8b9aa
Adam Dougherty <adamiandougherty@gmail.com>
2023-12-15 00:16:33 +0000

    Latest build

---
+++ 0.7.0.10/core/js/webworker/game.mjs
@@ -0,0 +1,4 @@
+import GameServer from "./GameServer.mjs";
+
+const game = new GameServer();
+export default game;
---
+++ 0.7.0.10/core/js/webworker/game.path.mjs
@@ -0,0 +1,4 @@
+import PathGameServer from "./PathGameServer.mjs";
+
+const game = new PathGameServer();
+export default game;
