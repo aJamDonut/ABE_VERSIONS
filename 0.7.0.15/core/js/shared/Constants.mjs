4acad7d603cf4740a571fda7b9289f940202a782
Adam Dougherty <adamiandougherty@gmail.com>
2024-01-28 13:40:47 +0000

    Latest build

---
+++ 0.7.0.15/core/js/shared/Constants.mjs
@@ -0,0 +1,25 @@
+const CONSTS = {};
+
+export {CONSTS};
+export default CONSTS;
+
+
+//JOB SEARCH PRIORITIES
+CONSTS.SEARCH_PRIORITIES = {
+    DROP_OFF_ITEMS: 0,
+    DROP_OFF_BODIES: 0,
+    CRAFT: 1,
+    BUILD: 1,
+    PICKUP_ITEMS: 2,
+    PICKUP_BODIES: 2,
+    FARM_RESOURCE: 3,
+}
+
+//JOB PRIORITIES, e.g. Cage vs Incinerator
+CONSTS.JOB_PRIORITIES = {
+    LOW: 3,
+    MED: 2,
+    HIGH: 1,
+    SPECIAL: 0, //Quests etc
+}
+
