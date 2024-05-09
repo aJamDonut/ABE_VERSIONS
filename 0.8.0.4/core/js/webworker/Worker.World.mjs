d6942035f9a7b57ca05ac15b886607880ab3f4f7
Adam Dougherty <adamiandougherty@gmail.com>
2024-05-10 00:32:00 +0100

    Latest build

---
+++ 0.8.0.4/core/js/webworker/Worker.World.mjs
@@ -0,0 +1,40 @@
+self._server = "World";
+import ABE from "./ABE.mjs";
+self.ABE = ABE;
+
+import game from "./game.mjs";
+
+import {bootStrap} from "./bootStrap.mjs";
+import {bootStrap as bootStrapLive} from "./bootStrap.live.mjs";
+
+import {isLive} from "../shared/Environment.mjs";
+const strapper = isLive() ? bootStrapLive : bootStrap;
+
+console.log("IS LIVE?", isLive());
+
+self.game = game;
+
+self.servers = game.servers;
+
+self.onmessage = async (response) => {
+	try {
+		await strapper((response) => {
+			try {
+				self.game.onMessage(response, postMessage);
+			} catch (e) {
+				console.error(e);
+			}
+		}, response);
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+self.postMessage({
+	server: self._server.toLowerCase(),
+	status: "ready"
+});
+
+Date.now = function () {
+	return game.ts;
+};
